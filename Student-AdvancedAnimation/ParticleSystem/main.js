c07267890ca618593e28fcd9c6ce929605f91906
ecarmona22 <42650005+ecarmona22@users.noreply.github.com>
2018-10-12 10:35:51 -0700

    finsih particle

--- Student-AdvancedAnimation/ParticleSystem/main.js
+++ Student-AdvancedAnimation/ParticleSystem/main.js
@@ -38,11 +38,3 @@ function makeParticleSystem(event){
   particleSystems.push(new ParticleSystem(event.clientX, event.clientY));
 
 }
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
