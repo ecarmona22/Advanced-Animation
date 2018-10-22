7e05ad41fa662ef4f15a8a18162e3806129a32d6
ecarmona22 <42650005+ecarmona22@users.noreply.github.com>
2018-10-22 10:30:37 -0700

    vehicle

--- Student-AdvancedAnimation/Flocking/Vehicle.js
+++ Student-AdvancedAnimation/Flocking/Vehicle.js
@@ -4,10 +4,19 @@ function Vehicle(x,y){//file has not been added to index.html
   this.acc = new JSVector(0,0);
   this.maxSpeed;//book = 4
   this.maxForce;//book = 0.1
+  this.desiredSeperation = 10;
+  this.otherVehicles = ();
+  //target  = average location
+  //
 }
-//left of beefore6.4 nature of code 
+//left of at example 6.7 group behavior: serpation working on first if statment 
 
-
+//function for cohesion (find average location for neighboring vehicles)
+// determine acceleration and call apply force
+//function for allignmentz(average velocity of all neighbors )
+//function for speration (steer away from really close vehicles )
+// sum of accelerations and giving weights,magnitude by normalizing then mulitplying by a coefficent
+//function checkedges(change direction of flock so it returns to screen  )
 };
 Vehicle.prototype.update = function () {
   this.vel.add(this.acc);
@@ -28,7 +37,17 @@ Vehicle.prototype.steer = function (target) {
   this.steer.limit(this.maxForce);
   this.applyforce(this.steer);
   //apply force by adding to loc? or vel?
+Vehicle.prototype.seperation = function () {
+  var sum = new JSVector(0,0);
+  var count = 0;
+  for(let i = 0;i<otherVehicles.length;i++){
+    var d = this.loc.distance(otherVehicles.loc);
+    if((d>0)&&(d<this.desiredSeperation)){
+      var diff =
+    }
+  }
 
+};
   Vehicle.prototype.render = function () {
     ctx.save();
     ctx.strokeStyle = 'rgba(0,0,0, .9)';
