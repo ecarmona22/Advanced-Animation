c8231f2d7fe58207e55c1bdb17a2258d2918b0c3
Elmer Carmona <790988@seq.org>
2018-12-07 12:28:42 -0800

    sqaure

--- Student-AdvancedAnimation/EcoSystem/creator.js
+++ Student-AdvancedAnimation/EcoSystem/creator.js
@@ -1,26 +1,11 @@
 function Creator(){
   this.vehicles = [];
   this.snakes = [];
-  //this.boid = [];
-  this.boid;
-  this.boidRadius = 30;
-  this.orbitor;
-  this.amountOfVehicles = 100; // amount of vehicles flocking
+  this.buddies = [];
+  this.amountOfBuddies = 3;
+  this.amountOfVehicles = 75; // amount of vehicles flocking
   this.amountOfSnakes = 4;// number of snakes
   this.numOfSeg = 8; //number of segments
-
-  this.ballCreater= function(){
-    var x = Math.random()*canvas.width;
-    var y = Math.random()*canvas.height;
-    var loc = new JSVector(x,y);
-    var dx =.02;
-    var dy = .02;
-    var vel = new JSVector(dx,dy);
-    var ax = .001;
-    var ay = .001;
-    var acc = new JSVector(ax,ay);
-    this.boid = new Ball(loc,vel,acc,30);
-  };
 
   this.makeCreatures();
 
@@ -34,8 +19,10 @@ Creator.prototype.makeCreatures = function () {
   for(let i = 0; i< this.amountOfSnakes;i++){
     this.snakes.push(new Snake(this.numOfSeg)); }
 
-    this.ballCreater();
-    this.orbitor = new Orbitor(this.boid);
+  for(let i = 0; i< this.amountOfBuddies;i++){
+    this.buddies.push(new Buddies());
+}
+
 
 };
 
@@ -47,6 +34,8 @@ Creator.prototype.run = function () {
     this.snakes[i].run();
 }
 
-  this.boid.run();
-  this.orbitor.run();
+for(let i = 0; i < this.buddies.length;i++){
+  this.buddies[i].run();
+}
+
 }
--- Student-AdvancedAnimation/EcoSystem/oribitor.js
+++ Student-AdvancedAnimation/EcoSystem/oribitor.js
@@ -1,8 +1,10 @@
-function Orbitor(otherBall){
+function Orbitor(otherBall,place){
   this.otherBall = otherBall;
-  this.radiusOfOrbitor = 10;
-  this.radiusFromBall = new JSVector(70,0);
-  this.angularVel = .05;
+  this.radiusOfOrbitor = 5;
+  this.postition = (Math.random()*4)*(Math.PI/2);
+  this.radius = Math.random()* (110-40)+40;
+  this.radiusFromBall = new JSVector(this.radius,this.postition);
+  this.angularVel = Math.random() * (.09 -.02)+.02;
 
   Orbitor.prototype.run = function(){
     this.update();
@@ -14,7 +16,10 @@ function Orbitor(otherBall){
     this.render();
   }
   Orbitor.prototype.render = function(){
-    var loc  = JSVector.addGetNew(this.radiusFromBall,this.otherBall.loc);
+    var temp = this.otherBall.loc.copy();;
+    temp.x+= 15;
+    temp.y-= 15;
+    var loc  = JSVector.addGetNew(this.radiusFromBall,temp);
     ctx.strokeStyle = 'rgba(0,0,0)';
     ctx.fillStyle = "rgba(2,54,244)";
     ctx.fill();
