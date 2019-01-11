f06dc61ec72403e98b0617db8da2a611f7febdb4
ecarmona22 <42650005+ecarmona22@users.noreply.github.com>
2019-01-11 10:35:25 -0800

    temp

--- semester 2/Lab 111/main.js
+++ semester 2/Lab 111/main.js
@@ -1,14 +1,15 @@
-'use strict';
+'use strict'
 window.onload = init;//  After the window has been loaded, go to init
 
 // global variables for canvas and context
 var canvas;
 var ctx;
-
-var particleSystems = [];
-
+var ball;
+var randomX;
+var randomY;
 
 function init(){
+  ball = [];
   //get the canvas
   canvas = document.getElementById('cnv');
   // Set the dimensions of the canvas
@@ -16,26 +17,37 @@ function init(){
   canvas.height = window.innerHeight;
   canvas.style.border = 'solid black 5px';
   canvas.style.backgroundColor = 'rgba(0,0,0, .9)';
-  canvas.addEventListener("click", makeParticleSystem);
+  canvas.addEventListener("click", moveCanvas);
   // get the context
   ctx = canvas.getContext('2d'); // This is the context
-  //ctx.translate(window.innerWidth/2,window.innerHeight/2)
-  //ctx.rotate(Math.PI/2)
-
+  makeBalls();
   animate();
 }
 
 function animate(){
+
+
   requestAnimationFrame(animate);
   ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
-  for(let i = 0; i<particleSystems.length;i++){
-    particleSystems[i].run();
-  }
-
+  for(let i = 0; i< ball.length;i++){
+      ball[i].run();
 }
 
 
-function makeParticleSystem(event){
-  particleSystems.push(new ParticleSystem(event.clientX, event.clientY));
 
-}
+   }
+
+   function makeBalls(){
+     for(let i = 0; i<8;i++){
+       randomX = Math.random()* (1900+1900)-1900;
+       randomY = Math.random() * (1000+1000)-1000;
+        ball.push(new Objects(randomX,randomY));
+     }
+
+   }
+
+   function moveCanvas(event){
+     ctx.save();
+     ctx.translate((canvas.width/2)-event.clientX,(canvas.height/2)-event.clientY);
+     ctx.restore();
+   }
